a1687adb649b822e3a274f09b95e460e0c723005
Kumar Gaurav <cse.18bcs2105@gmail.com>
2022-09-06 10:52:01 +0530

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+import NavBarComp from './Components/NavBarComp';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import MainPage from './Pages/MainPage';
+import CartPage from './Pages/CartPage';
+import LoginPage from './Pages/LoginPage';
+import SignupPage from './Pages/SignupPage';
+import SalesReportPage from './Pages/Admin/SalesReportPage';
+import StocksPage from './Pages/Admin/StocksPage';
+import Protected from './Protected';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <h1 className='shoplogo'>Happy Buying</h1>
+        <NavBarComp />
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/Signup' element={<SignupPage />} />
+          <Route path='/stocks' element={<StocksPage />} />
+          <Route path='/sales' element={<SalesReportPage />} />
+          <Route path='/cart' element={<Protected Component={CartPage} />} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
---
+++ src/Pages/Admin/StocksPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StocksPage = () => {
+  return (
+    <div>StocksPage</div>
+  )
+}
+
+export default StocksPage
